7f1253fedb3f84d0e88ce2a17f6f375cceeb2ee3
rdumaguin <rommeldumaguin@gmail.com>
2017-10-11 21:41:32 -0700

    Added 'Great_Number_Game' assignment

--- Python/Great_Number_Game/Great_Number_Game.py
+++ Python/Great_Number_Game/Great_Number_Game.py
@@ -1,25 +1,31 @@
-import random
 from flask import Flask, render_template, request, session, redirect  # Import Flask to allow us to create our app.
 app = Flask(__name__)    # Global variable __name__ tells Flask whether or not we are running the file
                          # directly, or importing it as a module.
 app.secret_key = 'SecretKey'
-# @app.route('/')          # The "@" symbol designates a "decorator" which attaches the following
-#                          # function to the '/' route. This means that whenever we send a request to
-#                          # localhost:5000/ we will run the following "hello_world" function.
+import random
 
 @app.route('/')
-def counter():
-  # session['count'] += 1
-  session['count'] = random.randrange(0,101) # random number between 0-100
-  return render_template('index.html')
+def main():
 
-@app.route('/plus2')
-def plus2():
-   session['count'] += 1
+    if 'target' not in session:
+        session['target'] = random.randrange(0,101)
+    return render_template('index.html')
+
+@app.route('/result', methods=['post'])
+def result():
+   if session['target'] == int(request.form['guess']):
+       session['result'] = 'correct'
+       session['end'] = 'true'
+   elif session['target'] > int(request.form['guess']):
+       session['result'] = 'low'
+   elif session['target'] < int(request.form['guess']):
+       session['result'] = 'high'
+   print session['result']
    return redirect('/')
 
 @app.route('/reset')
 def reset():
-   session['count'] = -1
+   session.pop('target')
+   session.pop('result')
    return redirect('/')
-app.run(debug=True)      # Run the app in debug mode.
+app.run(debug=True)     # Run the app in debug mode.
