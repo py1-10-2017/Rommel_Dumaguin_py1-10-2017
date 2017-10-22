bc073a80eb07c7defe9c82de747d53b903493692
rdumaguin <rommeldumaguin@gmail.com>
2017-10-21 21:01:39 -0700

    Updated 'Sakila' assignment

--- Python/MySQL/Email_Validation_with_DB/Email_Validation_with_DB.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM users
-INSERT INTO `email_validation_with_db`.`users` (`email`, `created_at`, `updated_at`) VALUES ('rommel.dumaguin', now(), now());
--- Python/MySQL/Sakila.sql
+++ Python/MySQL/Sakila.sql
@@ -26,11 +26,8 @@ Your query should return customer first name, last name, email, and address.
 SELECT customer.store_id, city.city_id, customer.first_name, customer.last_name, customer.email, address.address FROM customer
 LEFT JOIN address ON address.address_id = customer.address_id
 LEFT JOIN city ON city.city_id = address.city_id
-AND city.city_id = 1
-OR city.city_id = 42
-OR city.city_id = 312
-OR city.city_id = 459
 WHERE customer.store_id = 1
+AND city.city_id IN (1, 42, 312, 459);
 */
 /*
 #5. What query would you run to get all the films with a "rating = G" and "special feature = behind the scenes", joined by actor_id = 15? Your query should return the film title, description, release year, rating, and special feature. Hint: You may use LIKE function in getting the 'behind the scenes' part.
