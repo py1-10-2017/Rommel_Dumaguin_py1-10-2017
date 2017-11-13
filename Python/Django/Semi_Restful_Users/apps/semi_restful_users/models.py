52ce9548bc0029930d135ace5d9536acc6ace06c
rdumaguin <rommeldumaguin@gmail.com>
2017-11-13 00:02:00 -0800

    Added 'Courses' assignment

--- Python/Django/Semi_Restful_Users/apps/semi_restful_users/models.py
+++ Python/Django/Semi_Restful_Users/apps/semi_restful_users/models.py
@@ -40,8 +40,8 @@ class UserManager(models.Manager):
         return errors
 
 class User(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add = True)
